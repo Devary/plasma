32792ef73318c617dedcb0391bb9a6de7e12d36c
hfekher <hfekher@gmail.com>
2020-03-13 18:05:17 +0100

    v1.0.0

--- pl/mapper/src/main/java/AbstractMapper.java
+++ pl/mapper/src/main/java/AbstractMapper.java
@@ -1,3 +1,44 @@
-public class AbstractMapper {
+import files.AbstractFile;
 
+import java.util.HashMap;
+
+public class AbstractMapper implements IAbstractMapper{
+    public HashMap<Integer, AbstractFile> files = null;
+    public String projectPath = null;
+
+
+    public void initProject() {
+
+    }
+
+    public void getAllFiles() {
+
+    }
+
+    public void getPersistenceFiles() {
+
+    }
+
+    public HashMap<Integer, AbstractFile> getFiles() {
+        return files;
+    }
+
+    public void setFiles(HashMap<Integer, AbstractFile> files) {
+        this.files = files;
+    }
+
+    public String getProjectPath() {
+        return projectPath;
+    }
+
+    public void setProjectPath(String projectPath) {
+        this.projectPath = projectPath;
+    }
+    public void setFile(AbstractFile abstractFile)
+    {
+        if (abstractFile != null)
+        {
+            this.files.put(this.files.size()+1,abstractFile);
+        }
+    }
 }
--- pl/mapper/src/main/java/IAbstractMapper.java
+++ pl/mapper/src/main/java/IAbstractMapper.java
@@ -1,3 +1,11 @@
+import files.AbstractFile;
+
+import java.util.HashMap;
+
 public interface IAbstractMapper {
+
+    public void initProject();
+    public void getAllFiles();
+    public void getPersistenceFiles();
 
 }
