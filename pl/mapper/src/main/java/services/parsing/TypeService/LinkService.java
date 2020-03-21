06f2b0fec09c5edbed0bdb1c3e3e609327bc418d
Fakher H <hfekher@gmail.com>
2020-03-21 14:44:20 +0100

    update 2.0.1 :  class loading from another project :: test on solife

--- pl/mapper/src/main/java/services/parsing/TypeService/LinkService.java
+++ pl/mapper/src/main/java/services/parsing/TypeService/LinkService.java
@@ -2,27 +2,34 @@ package services.parsing.TypeService;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import projects.ProjectFile;
 import services.parsing.ParsingService;
 
-public class LinkService {
+public class LinkService extends ParsingService{
     Node node;
     private Document xmlDocument;
     private boolean checkForObjects;
+    private ProjectFile projectFile = null;
 
     public LinkService(Node node,Document xmlDocument) {
         this.node = node;
         this.xmlDocument = xmlDocument;
         this.checkForObjects = true;
+        this.projectFile= getFile();
+
     }
 
     public Class getCollectionType() {
         if (!checkForObjects)
         try {
             if (createNodeAndCheckForExistence("collectionType")!=null)
-                return Class.forName(this.node
-                    .getAttributes()
-                    .getNamedItem("collectionType")
-                    .getNodeValue());
+            {
+                Class cls = this.loadClassFromTargetProject(this.projectFile,this.node
+                        .getAttributes()
+                        .getNamedItem("collectionType")
+                        .getNodeValue());
+                return Class.forName(cls.getName());
+            }
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -66,5 +73,10 @@ public class LinkService {
     }
     private boolean checkIfNodeExists(Node node) {
         return null != node;
+    }
+
+    @Override
+    public Class loadClassFromTargetProject(ProjectFile projectFile, String classPath) {
+        return super.loadClassFromTargetProject(projectFile, classPath);
     }
 }
